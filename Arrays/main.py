143876c5a911f262cc078ce9a687a1e3205fccef
Kailey Ruhlman <kar253@pitt.edu>
2022-04-10 14:32:33 -0400

    Arrays code

---
+++ Arrays/main.py
@@ -0,0 +1,11 @@
+#makeing a list of the names of the characters in a DnD party.
+DnDNames = ["Angelus", "Phyrra", "Lemen", "Dae", "Captian"]
+
+#add a name to the array
+DnDNames.append("Choji")
+
+#sort the names in alphabetical order
+DnDNames.sort()
+
+#print all the names in the array/list
+print(DnDNames)
